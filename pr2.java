695e5938f7af1def6f3752f520d0a12d9c3b5482
HARSH DUDHAT <106002779+HARSHDUDHAT07@users.noreply.github.com>
2022-09-12 12:56:31 +0530

    Add files via upload

---
+++ pr2.java
@@ -0,0 +1,20 @@
+// Prepared by HARSH DUDHAT 21CE026
+// WAP to generate user defined exception using “throw” and “throws” keyword.
+public class pr2 {
+    static void fun() {
+        try {
+            throw new NullPointerException("demo");
+        } catch (NullPointerException e) {
+            System.out.println("Caught inside fun().");
+            throw e; // rethrowing the exception
+        }
+    }
+
+    public static void main(String args[]) {
+        try {
+            fun();
+        } catch (NullPointerException e) {
+            System.out.println("Caught in main.");
+        }
+    }
+}
